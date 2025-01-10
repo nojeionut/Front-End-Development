19d9a2e3f5bd03f4889cbfe5b537dfe2af3fc7e2
ionutnoje <65008544+ionutnoje@users.noreply.github.com>
2025-01-10 15:53:19 +0200

    basics about the console in javascript

---
+++ Javascript/Modern JavaScript/1. Variabile si tipuri de date/javascript.js
@@ -0,0 +1,22 @@
+console.log("Hello, Noje learns javascript here!");
+console.log("My favourite number is :" , 21);
+console.log(21, 21.5, 21.5e10, 21.5e-10);
+
+var a = 21;
+var b = 21;
+var suma = a + b;
+console.log(suma);
+
+console.log(suma + 1);
+
+//alert("Hello, Noje learns javascript here!");
+
+console.error("This is an error message!");
+console.warn("This is a warning message!");
+
+
+console.group("Group 1");
+console.log("Hello, Noje learns javascript here!");
+console.log("My favourite number is :" , 21);
+console.log(21, 21.5, 21.5e10, 21.5e-10);
+console.groupEnd();
